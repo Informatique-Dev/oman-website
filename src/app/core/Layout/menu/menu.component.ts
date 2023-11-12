4a73fc0657504e803f83c9dfbafb5065506d2281
Zinab Hashem <zadel@informatique@gmail.com>
2023-11-12 11:39:57 +0200

    edit

--- src/app/core/Layout/menu/menu.component.ts
+++ src/app/core/Layout/menu/menu.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html'
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
  constructor(private router: Router) {
